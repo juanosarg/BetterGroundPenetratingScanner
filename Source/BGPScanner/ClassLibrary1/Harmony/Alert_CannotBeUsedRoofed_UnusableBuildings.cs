5b987cfdc2781c0bb7a179bd675dfe2f15c7fdd0
KylianB <kylian.balde@gmail.com>
2023-01-15 14:39:17 +0100

    fix alert showing with under roof on

---
+++ Source/BGPScanner/ClassLibrary1/Harmony/Alert_CannotBeUsedRoofed_UnusableBuildings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BGPScanner
+{
+    [HarmonyPatch(typeof(Alert_CannotBeUsedRoofed))]
+    [HarmonyPatch("UnusableBuildings", MethodType.Getter)]
+    public static class Alert_CannotBeUsedRoofed_UnusableBuildings
+    {
+        public static void Postfix(ref List<Thing> __result)
+        {
+            List<Thing> result = new List<Thing>();
+            for (int i = 0; i < __result.Count; i++)
+            {
+                var r = __result[i];
+                if (r.def.defName == "GroundPenetratingScanner" && BGPScannerMod.settings.allowUnderRoof)
+                    continue;
+
+                result.Add(r);
+            }
+            __result = result;
+        }
+    }
+}
